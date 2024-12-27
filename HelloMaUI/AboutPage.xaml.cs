d66bf50a53e5903cecdb4165414d2e909cf107ad
leo yang <1772081518@qq.com>
2024-12-27 15:56:53 +0800

    添加项目文件。

---
+++ HelloMaUI/AboutPage.xaml.cs
@@ -0,0 +1,17 @@
+using Android.App;
+using AndroidX.AppCompat.App;
+
+namespace HelloMaUI;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		
+    }
+}
